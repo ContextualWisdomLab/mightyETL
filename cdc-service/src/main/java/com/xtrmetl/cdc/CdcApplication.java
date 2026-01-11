48d60e57cf5def3dee172a61fb8881d4212a8642
Seongho Bae <me@seonghobae.me>
2026-01-11 19:12:49 +0900

    chore: inline XtrmetlProperties reference

--- cdc-service/src/main/java/com/xtrmetl/cdc/CdcApplication.java
+++ cdc-service/src/main/java/com/xtrmetl/cdc/CdcApplication.java
@@ -1,6 +1,5 @@
 package com.xtrmetl.cdc;
 
-import com.xtrmetl.cdc.config.XtrmetlProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -9,7 +8,7 @@ import org.springframework.kafka.annotation.EnableKafka;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableKafka
-@org.springframework.boot.context.properties.EnableConfigurationProperties(XtrmetlProperties.class)
+@org.springframework.boot.context.properties.EnableConfigurationProperties(com.xtrmetl.cdc.config.XtrmetlProperties.class)
 public class CdcApplication {
     public static void main(String[] args) {
         SpringApplication.run(CdcApplication.class, args);
