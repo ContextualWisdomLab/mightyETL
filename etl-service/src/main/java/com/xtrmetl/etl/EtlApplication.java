46b9e7799b7ad946d2d802db8ff7069c21b4c795
Seongho Bae <me@seonghobae.me>
2026-08-03 12:08:44 +0900

    feat(etl): bind processing safety properties

--- etl-service/src/main/java/com/xtrmetl/etl/EtlApplication.java
+++ etl-service/src/main/java/com/xtrmetl/etl/EtlApplication.java
@@ -1,5 +1,6 @@
 package com.xtrmetl.etl;
 
+import com.xtrmetl.etl.config.EtlProcessingProperties;
 import com.xtrmetl.etl.connector.ConnectorProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,12 +9,20 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.retry.annotation.EnableRetry;
 
+/**
+ * Boots the independently deployable mightyETL processing service.
+ */
 @SpringBootApplication(scanBasePackages = {"com.xtrmetl.etl", "com.xtrmetl.common"})
 @EnableDiscoveryClient
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 @EnableRetry
-@EnableConfigurationProperties(ConnectorProperties.class)
+@EnableConfigurationProperties({ConnectorProperties.class, EtlProcessingProperties.class})
 public class EtlApplication {
+
+    private EtlApplication() {
+        // Application entry point only.
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(EtlApplication.class, args);
     }
